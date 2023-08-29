397d3adb942510314066edaea11872929ddd2c57
nitesh-19 <niteshgohil25@gmail.com>
2023-08-29 18:53:40 +0530

    bad code-ignore

--- main.py
+++ main.py
@@ -15,17 +15,20 @@ list_of_year = [int(index.replace(" Population", "")) for index in population_in
 my_data = pd.DataFrame(data=list_of_year,
                        columns=["Year"])
 my_data["Population"] = list_of_population
-my_data["Population"] = round(my_data["Population"] * 10 ** -8)  # Converting population to in Millions
-my_data["Year"] = round(my_data["Year"] + 30 - 2000)  # Converting population to in Millions
-
+my_data["Population"] = round(my_data["Population"] * 10 ** -8)  # Converting population to small numbers
+print(my_data)
+# my_data["Year"] = round(my_data["Year"] - my_data["Year"][len(my_data)-1])  # Converting Year to small numbers
+# subtract_value = my_data["Year"][0] - my_data["Year"][len(my_data)-1]
+# print(subtract_value)
 print(my_data)
 
 # def cost_function():
-cost_var = 1000000000000
+cost_var = 1000
 m = len(my_data)
-w = 0
-b = 0
-alpha =0.001
+w = 5
+b = 10
+alpha = 0.001
+models = []
 
 
 def line_function(x, w, b):
@@ -38,8 +41,14 @@ def cost_function(w, b):
     summation = 0
     for i in range(0, m):
         y_cap = line_function(my_data.Year[i], w, b)
-        summation += (y_cap - my_data.Population[i]) ** 2
-    cost = summation / (2 * m)
+        temp_summation = int(y_cap - my_data.Population[i]) * int(y_cap - my_data.Population[i])
+        summation += int(temp_summation)
+        int(summation)
+    if summation > 10000000000:
+        summation *= 10000000000000000000
+        round(summation, 10000)
+        int(summation)
+    cost = int(summation // (2 * m))
     cost_var = cost
     return cost
 
@@ -52,24 +61,36 @@ def gradient_descent():
     for i in range(0, m):
         y_cap = line_function(my_data.Year[i], w, b)
         summation_w += (y_cap - my_data.Population[i]) * my_data.Year[i]
+        int(summation_w)
     for i in range(0, m):
         y_cap = line_function(my_data.Year[i], w, b)
         summation_b += (y_cap - my_data.Population[i])
+        int(summation_b)
     summation_w /= m
+    int(summation_w)
     summation_b /= m
-    w_temp = w - alpha * summation_w
-    b_temp = b - alpha * summation_b
+    int(summation_b)
+    w_temp = int(w - alpha * summation_w)
+
+    b_temp = int(b - alpha * summation_b)
     w = w_temp
     b = b_temp
     cost_function(w, b)
 
 
-def model():
-    pass
+def model(w, b, alpha, cost_var):
+    global models
+    models.append({"Slope": w, "Y-Intercept": b, "Alpha": alpha, "Cost": cost_var})
+    with open("models_log.txt", "a") as file:
+        file.write(str(models) + "\n")
+    print(models)
+    print(cost_var)
 
 
-while cost_var > 0.1:
+while cost_var > 1:
     gradient_descent()
+
+# my_data["Year"] = round(my_data["Year"] + subtract_value)
 
 x1_coordinate = my_data.Year[len(my_data) - 1]
 y1_coordinate = line_function(x1_coordinate, w, b)
@@ -80,3 +101,4 @@ my_data.plot.scatter(x="Year", y="Population", ylabel="Population in Millions")
 
 plt.plot([x1_coordinate, x2_coordinate], [y1_coordinate, y2_coordinate])
 plt.show()
+model(w, b, alpha, cost_var)
