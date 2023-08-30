4918b1641bd75242eba67548e336c6fb0654501c
nitesh-19 <niteshgohil25@gmail.com>
2023-08-30 12:19:54 +0530

    Added decimal precision to avoid very precise floats

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import math
 
 DATA_FILE_PATH = r".\data\world_population.csv"
 COUNTRY = "India"
@@ -15,17 +14,22 @@ list_of_year = [int(index.replace(" Population", "")) for index in population_in
 
 my_data = pd.DataFrame(data=list_of_year, columns=["Year"])
 my_data["Population"] = list_of_population
-my_data["Population"] = round(
-    my_data["Population"] * 10 ** -8)  # Converting population to small numbers for easy calculation
-print(my_data)
-my_data["Year"] = round(my_data["Year"] - my_data["Year"][len(my_data) - 1])  # Converting Year to small numbers
 
-# def cost_function():
+# Converting population to small numbers for easy calculation
+my_data["Population"] = round(my_data["Population"] * 10 ** -8)
+print(my_data)
+
+# Converting Year to small numbers for easy calculation
+my_data["Year"] = round(my_data["Year"] - my_data["Year"][len(my_data) - 1])
+
 cost_var = 1000
 m = len(my_data)
 w = 0
-b = 5
-alpha = 0.001
+b = 0
+ALPHA = 0.001
+ITERATIONS_LIMIT = 5000
+COST_LIMIT = 0.1
+DECIMAL_PRECISION = 8
 models = []
 
 
@@ -39,8 +43,11 @@ def cost_function(w, b):
     summation = 0
     for i in range(0, m):
         y_cap = line_function(my_data.Year[i], w, b)
+        y_cap = round(y_cap, DECIMAL_PRECISION)
         summation += (y_cap - my_data.Population[i]) ** 2
+        summation = round(summation, DECIMAL_PRECISION)
     cost = summation / (2 * m)
+    cost = round(cost, DECIMAL_PRECISION)
     cost_var = cost
     return cost
 
@@ -48,37 +55,43 @@ def cost_function(w, b):
 def gradient_descent():
     global w
     global b
+    global no_of_iterations
     summation_w = 0
     summation_b = 0
     for i in range(0, m):
         y_cap = line_function(my_data.Year[i], w, b)
+        y_cap = round(y_cap, DECIMAL_PRECISION)
         summation_w += (y_cap - my_data.Population[i]) * my_data.Year[i]
+        summation_w = round(summation_w, DECIMAL_PRECISION)
     for i in range(0, m):
         y_cap = line_function(my_data.Year[i], w, b)
+        y_cap = round(y_cap, 4)
         summation_b += (y_cap - my_data.Population[i])
+        summation_b = round(summation_b, DECIMAL_PRECISION)
     summation_w /= m
     summation_b /= m
-    w_temp = w - alpha * summation_w
-    b_temp = b - alpha * summation_b
+    w_temp = w - ALPHA * summation_w
+    w_temp = round(w_temp, DECIMAL_PRECISION)
+    b_temp = b - ALPHA * summation_b
+    b_temp = round(b_temp, DECIMAL_PRECISION)
     w = w_temp
-
     b = b_temp
     cost_function(w, b)
 
 
-def model(w, b, alpha, cost_var):
+def model(w, b, alpha, cost_var, iterations):
     global models
-    models.append({"Slope": w, "Y-Intercept": b, "Alpha": alpha, "Cost": cost_var})
+    models.append({"Slope": w, "Y-Intercept": b, "Alpha": alpha, "Cost": cost_var, "Number of iterations": iterations})
     with open("models_log.txt", "a") as file:
         file.write(str(models) + "\n")
     print(f"Model: {models}")
     print(f"Cost: {cost_var}")
 
 
-while cost_var > 0.1:
+no_of_iterations = 0
+while cost_var > COST_LIMIT and no_of_iterations < ITERATIONS_LIMIT:
     gradient_descent()
-
-# my_data["Year"] = round(my_data["Year"] + subtract_value)
+    no_of_iterations += 1
 
 x1_coordinate = my_data.Year[len(my_data) - 1]
 y1_coordinate = line_function(x1_coordinate, w, b)
@@ -89,4 +102,4 @@ my_data.plot.scatter(x="Year", y="Population", ylabel="Population in Millions")
 
 plt.plot([x1_coordinate, x2_coordinate], [y1_coordinate, y2_coordinate])
 plt.show()
-model(w, b, alpha, cost_var)
+model(w, b, ALPHA, cost_var, no_of_iterations)
