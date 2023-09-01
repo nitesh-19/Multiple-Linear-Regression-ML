c49f23019f34b19dd4f2e67db32299701da23558
nitesh-19 <niteshgohil25@gmail.com>
2023-09-01 22:38:50 +0530

    One feature regression verified to be working

--- population_predictor.py
+++ population_predictor.py
@@ -1,11 +1,10 @@
 from linear_regression import LinearRegression
 
-trainer = LinearRegression(ITERATION_SAMPLING_VALUE=5, ALPHA=0.0000005, ITERATIONS_LIMIT=391)
+trainer = LinearRegression(ITERATION_SAMPLING_VALUE=5, ALPHA=0.005, ITERATIONS_LIMIT=100000)
 trainer.DATA_PATH = r".\data\Fish.csv"
 trainer.x_index_list = [5]
 trainer.y_index = 1
 trainer.should_scale_data = False
-trainer.w = [0.90497442]
-trainer.b = [0.90497442]
+
 trainer.run_trainer()
 
