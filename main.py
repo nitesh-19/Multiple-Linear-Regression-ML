d820f5c620ccac5c037795bf68ed9511fc9a1aa0
nitesh-19 <niteshgohil25@gmail.com>
2023-09-02 23:21:04 +0530

    Updated print text

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ data_path = r".\data\insurance.csv"
 feature_columns = [0, 2, 3, 4]
 target_column = 6
 trainer = LinearRegression(DATA_PATH=data_path, feature_columns_index=feature_columns,
-                           target_column_index=target_column, ALPHA=0.5, create_test_set=True)
+                           target_column_index=target_column, ALPHA=0.5, create_test_set=False, ITERATIONS_LIMIT=50)
 
-parameters = [53,33.25,0,0]
-prediction = predict_with_model(parameters)
-print(prediction)
+parameters = [53, 33.25, 0, 0]
+target_prediction = predict_with_model(parameters)
+print(f"Prediction for the input features {parameters} is {target_prediction}")
