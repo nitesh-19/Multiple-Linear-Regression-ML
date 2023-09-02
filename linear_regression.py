01a3bfdfbe832b269905da6ea2c2b5deca27a7c3
nitesh-19 <niteshgohil25@gmail.com>
2023-09-02 23:00:19 +0530

    All functions working

--- linear_regression.py
+++ linear_regression.py
@@ -4,16 +4,23 @@ import matplotlib.pyplot as plt
 from random import sample
 
 
-# TODO - Test the testset creator
-# TODO - Create separate csv for test and trainging data
-
-
 def linear_equation(x, w, b):
     return np.dot(x, w) + b
 
 
+def predict_with_model(x):
+    x = np.array(x)
+    with open("fully_trained_models.txt", "r") as file:
+        lines = file.readlines()
+        parameter_dict = eval(lines[-1])
+    w = np.array(eval(parameter_dict["Slope"]))
+    b = np.array(eval(parameter_dict["Y - Intercept"]))
+    return linear_equation(x, w, b)
+
+
 def save_model(file_path, w, b, alpha, cost_var, iterations):
-    models_dict = {"Slope": str(w).replace(" ", ", "), "Y-Intercept": str(b), "Alpha": alpha, "Cost": str(cost_var),
+    models_dict = {"Slope": (str(w).replace(" ", ",")).replace("[,", "["), "Y - Intercept": str(b), "Alpha": alpha,
+                   "Cost": str(cost_var),
                    "Number of iterations": iterations,
                    }
     with open(file_path, "a") as file:
@@ -50,6 +57,7 @@ class LinearRegression:
         print("Do you want to resume training the last interrupted model?")
         response = input("Press 'y' to load the last model weights or press 'n' to set the weights to default: ")
         if response == "y":
+            self.create_test_set = False
             self.get_last_model()
             input("Press any key to start training: ")
 
@@ -64,7 +72,7 @@ class LinearRegression:
             lines = file.readlines()
             parameter_dict = eval(lines[-1])
         self.w = np.array(eval(parameter_dict["Slope"]))
-        self.b = np.array(eval(parameter_dict["Y-Intercept"]))
+        self.b = np.array(eval(parameter_dict["Y - Intercept"]))
         self.no_of_iterations = parameter_dict["Number of iterations"]
         # self.ALPHA = parameter_dict["Alpha"]
         print(f"Scaled w = {self.w}, b = {self.b}, Alpha = {self.ALPHA}")
@@ -72,17 +80,14 @@ class LinearRegression:
     def test_set_creator(self, data, percent_of_data=20):
         length_of_data = len(data)
 
-        # list_of_random_index = [319, 277, 405, 79, 129, 164, 501, 72, 369, 170, 349, 238, 358, 57, 476, 155, 389, 154,
-        #                         430, 1, 306, 211, 58, 222, 433, 293, 378, 138, 16, 359, 390, 152, 283, 224, 25, 150,
-        #                         226, 457, 438, 149, 523, 60, 499, 511, 93, 146, 371, 53, 339, 253, 61, 230, 243, 105,
-        #                         294, 370, 46, 397, 244, 402, 208, 354, 410, 478, 41, 374, 414, 325, 252, 514, 524, 51,
-        #                         196, 116, 431, 239, 122, 332, 304, 251, 26, 28, 109, 89, 262, 347, 417, 20, 449, 321,
-        #                         331, 80, 91, 183, 452, 443, 212, 409, 199, 432, 219, 161, 117, 135, 133, 147, 263, 482,
-        #                         483]
+        # list_of_random_index =
 
         list_of_random_index = sample(range(0, length_of_data), round(length_of_data * percent_of_data / 100))
         test_set = pd.DataFrame(data=data, index=list_of_random_index)
         data.drop(index=list_of_random_index, inplace=True)
+        test_set.to_csv("test_set.csv")
+        data.to_csv("training_set.csv")
+
         self.test_set = test_set
         return data
 
@@ -95,7 +100,7 @@ class LinearRegression:
         elif unscale:
             for i in range(0, len(self.columns)):
                 self.working_data[self.columns[i]] = self.working_data[self.columns[i]] * self.scale_factors[i]
-            self.b = self.b * self.scale_factors[-1]
+            # self.b = self.b * self.scale_factors[-1]
             for index in range(0, len(self.columns) - 1):
                 self.w[index] = self.w[index] * self.scale_factors[-1] / self.scale_factors[index]
 
--- population_predictor.py
+++ population_predictor.py
@@ -1,8 +1,11 @@
-from linear_regression import LinearRegression
+from linear_regression import LinearRegression, predict_with_model
 
-data_path = r".\data\Housing.csv"
-feature_columns = [1, 2]
-target_column = 0
+data_path = r".\data\insurance.csv"
+feature_columns = [0, 2, 3, 4]
+target_column = 6
 trainer = LinearRegression(DATA_PATH=data_path, feature_columns_index=feature_columns,
-                           target_column_index=target_column, ALPHA=0.8)
+                           target_column_index=target_column, ALPHA=0.5, create_test_set=True)
 
+parameters = [53,33.25,0,0]
+prediction = predict_with_model(parameters)
+print(prediction)
